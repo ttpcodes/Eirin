18a7f5f79e1d678bca6166968c4f4e2c534bdb0a
Michael William Le Nguyen <michael@mail.ttp.codes>
2020-04-09 23:07:55 -0700

    Add CGI logic

---
+++ cgi.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from eirin import app
+
+from json import load
+from os import environ
+from wsgiref.handlers import CGIHandler
+
+with open('config.json') as fp:
+    config = load(fp)
+    environ['SCRIPT_NAME'] = config['root']
+CGIHandler().run(app)
